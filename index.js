c2fd58f1b4082cee06b64784c65b72ceccd1ede7
panoramine <camila.c.a.salles@gmail.com>
2021-09-17 20:09:40 -0700

    sums from a to b recursively

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
+// base case => fromN === toN
+// recursive case => fromN < toN
 
 function sum(fromN, toN) {
-  // Sum all the values from fromN up to toN
-}
+  if (fromN === toN) {
+    return fromN;
+  }
+  while (fromN <= toN) {
+    console.log(fromN);
+    return fromN + sum(fromN + 1, toN);
+  }
+};
+
+console.log(sum(3, 3))
+
 
 module.exports = sum;
